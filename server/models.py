7b78842b4a5915a1b793d3f451385fbace9db8f8
Joseph <joseph.dena@student.moringaschool.com>
2024-04-09 19:38:58 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -10,14 +10,31 @@ class Author(db.Model):
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+     # All authors have a name
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Name field is required.")
+        
+        existing_author = db.session.query(Author.id).filter_by(name=name).first()
+        if existing_author:
+            raise ValueError("Name must be unique.")
+        return name
 
-    # Add validators 
+    # Author phone numbers are exactly ten digits
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number
+    
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
-
-class Post(db.Model):
-    __tablename__ = 'posts'
+    
+    class Post(db.Model):
+        __tablename__ = 'posts'
     
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -26,8 +43,37 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Title field is required.")
+        
+         # Add a custom validator to the Post model that ensures the title is sufficiently clickbait-y. The validator should add a validation error if the title does not contain:
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clickbait found")
+        return title
+    
+     # Post content is at least 250 characters long
+    @validates('content', 'summary')
+    def validate_length(self, key, string):
+        if( key == 'content'):
+            if len(string) < 250:
+                raise ValueError("Post content must be greater than or equal 250 characters long.")
+            
+        # Post summary is a maximum of 250 characters
+        if( key == 'summary'):
+            if len(string) > 250:
+                raise ValueError("Post summary must be less than or equal to 250 characters long.")
+        return string
 
-    # Add validators  
+    # Post category is either Fiction or Non-Fiction
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError("Category must be Fiction or Non-Fiction.")
+        return category
 
 
     def __repr__(self):
